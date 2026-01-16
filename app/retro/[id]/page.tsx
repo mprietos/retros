dd4536e370475d51230f4c730885f3f9b2f7588b
Miguel Prieto <miguelprieto@MacBook-Pro-de-Miguel.local>
2026-01-16 09:14:21 +0100

    routes

--- app/retro/[id]/page.tsx
+++ app/retro/[id]/page.tsx
@@ -1,6 +1,8 @@
+
 import { getSnapshot } from "@/lib/store";
 import RetroBoard from "@/components/RetroBoard";
 import { notFound } from "next/navigation";
+export const dynamic = "force-dynamic"
 
 export default function RetroPage({ params }: { params: { id: string } }) {
   const snap = getSnapshot(params.id);
